a85b90d044858647f628decbce4ef2203cabb73c
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-07-27 22:31:33 -0300

    Implemented initial code in Bubble sort classes

--- src/RefactoringCleanCode/BubbleSortRefactoring.java
+++ src/RefactoringCleanCode/BubbleSortRefactoring.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package RefactoringCleanCode;
 
-/**
- *
- * @author User
- */
-public class BubbleSortRefactoring {
-    
+// Implementação otimizada em Java do Bubble sort
+// Código extraído de https://www.geeksforgeeks.org/bubble-sort/
+
+//O método orderArrayWithBubbleSort precisa de ajustes, pois ele tem muitas responsabilidades
+//não gostei da variável boolean trocado e nem a variável temp;
+//remover comentários que não são necessários -- ok
+//Analisar se não é possível criar uma nova classe as características de um elemento que está sendo ordenado
+//reavaliar os nomes das variáveis 
+//Relembrar o funcionamento do BubbleSort
+//Arrumar a declaração do int i no método printArray -- ok
+//Criar testes unitários antes de refatorar um método para validar se as mudanças não vão mudar o funcionamento do método
+//O nome dos parâmetros não está legal nos métodos orderArrayWithBubbleSort -- ok
+
+class BubbleSort {
+
+    static void orderArrayWithBubbleSort(int[] arrayToOrder)
+    {
+        int arraySize = arrayToOrder.length;
+        
+        int i, j, temp;
+        boolean trocado;
+        for (i = 0; i < arraySize - 1; i++) {
+            trocado = false;
+            for (j = 0; j < arraySize - i - 1; j++) {
+                if (arrayToOrder[j] > arrayToOrder[j + 1]) {
+
+                    temp = arrayToOrder[j];
+                    arrayToOrder[j] = arrayToOrder[j + 1];
+                    arrayToOrder[j + 1] = temp;
+                    trocado = true;
+                }
+            }
+
+            if (trocado == false)
+                break;
+        }
+    }
+
+    static void printArray(int arr[])
+    {
+        for (int i = 0; i < arr.length; i++) System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+        int[] arrayToOrder = { 64, 34, 25, 12, 22, 11, 90 };
+        orderArrayWithBubbleSort(arrayToOrder);
+        System.out.println("Array ordenado: ");
+        printArray(arrayToOrder);
+    }
 }
+
+// This code is contributed
+// by Nikita Tiwari.
+
--- src/WithoutRefactoring/BubbleSort.java
+++ src/WithoutRefactoring/BubbleSort.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package WithoutRefactoring;
 
-/**
- *
- * @author User
- */
-public class BubbleSort {
-    
+// Código extraído de https://www.geeksforgeeks.org/bubble-sort/
+
+//O método bubbleSort precisa de ajustes, pois ele tem muitas responsabilidades
+//não gostei da variável boolean trocado e nem a variável temp;
+//remover comentários que não são necessários
+//Analisar se não é possível criar uma nova classe as características de um elemento que está sendo ordenado
+//reavaliar os nomes das variáveis
+//Relembrar o funcionamento do BubbleSort
+//Arrumar a declaração do int i no método printArray
+//Criar testes unitários antes de refatorar um método para validar se as mudanças não vão mudar o funcionamento do método
+//O nome dos parâmetros não está legal nos métodos bubbleSort
+
+class BubbleSort {
+
+    // An optimized version of Bubble Sort
+    static void bubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean trocado;
+        for (i = 0; i < n - 1; i++) {
+            trocado = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    trocado = true;
+                }
+            }
+
+            // If no two elements were
+            // trocado by inner loop, then break
+            if (trocado == false)
+                break;
+        }
+    }
+
+    // Function to print an array
+    static void printArray(int arr[], int size)
+    {
+        int i;
+        for (i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver program
+    public static void main(String args[])
+    {
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        int n = arr.length;
+        bubbleSort(arr, n);
+        System.out.println("Array ordenado: ");
+        printArray(arr, n);
+    }
 }
+
+// This code is contributed
+// by Nikita Tiwari.
